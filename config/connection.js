3e930856606d79b173582ad67686c16caaeebcc6
jaguar8476 <jim.a.gallagher@gmail.com>
2019-08-18 15:59:46 -0600

    upload

---
+++ config/connection.js
@@ -0,0 +1,27 @@
+var mysql = require("mysql");
+
+var connection;
+
+// Connect with JawsDB database on our Heroku app
+// 	If not found, then run locally
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	connection = mysql.createConnection({
+	  host: "localhost",
+	  user: "root",
+	  password: "Password01",
+	  database: "burgers_db"
+	});
+}
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+
+module.exports = connection;
